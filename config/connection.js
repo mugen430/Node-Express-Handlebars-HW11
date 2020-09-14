df86a84a30fc462377948806bd3b6245f4ecad66
mugen 430 <acampado.maverick@gmail.com>
2020-09-14 02:14:46 -0500

    updating

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-  var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.PASSWORD,
-    database: "burger_db"
-  });
-};
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "burger_db"
+});
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
